16b0ebf663b5d4117344bcabf5185a0ae3474178
Marchons <d1286360646@outlook.com>
2024-12-06 15:31:47 +0800

    delete unnecessary files
    Signed-off-by: Marchons <d1286360646@outlook.com>

--- uploadCloudFunction.bat
+++
@@ -1,1 +0,0 @@
-"D:\Program Files\微信web开发者工具\cli.bat" cloud functions deploy --e schoolmap-7gbh91vx48c69c86 --n quickstartFunctions --r --project "D:\school\github\MarketingPlatform" --report_first --report
