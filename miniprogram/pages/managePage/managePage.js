30d641ba86e196d63c6595712823637263246c10
JasonChanPy <2282727499@qq.com>
2024-12-16 20:17:04 +0800

    feat: 1. 新增产品详情页面、需求详情页、管理主页、管理页

---
+++ miniprogram/pages/managePage/managePage.js
@@ -0,0 +1,201 @@
+// pages/managePage/managePage.js
+import Dialog from '@vant/weapp/dialog/dialog';
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    adminUsers: [
+      {_id: '1', name: '李一', phone: '12345678901', type: 2, avatarUrl: '/images/admin-icon.png'},
+      {_id: '2', name: '陈二', phone: '12345678901', type: 2, avatarUrl: '/images/admin-icon.png'},
+      {_id: '3', name: '张三', phone: '12345678901', type: 2, avatarUrl: '/images/admin-icon.png'},
+    ],    
+    normalUsers: [
+      {_id: '4', name: 'GoGo', phone: '12345678901', type: 1, avatarUrl: '/images/admin-icon.png'},
+      {_id: '5', name: 'Jeny', phone: '12345678901', type: 0, avatarUrl: '/images/admin-icon.png'},
+      {_id: '6', name: 'Tony', phone: '12345678901', type: 1, avatarUrl: '/images/admin-icon.png'},
+      {_id: '1', name: '李一', phone: '12345678901', type: 2, avatarUrl: '/images/admin-icon.png'},
+      {_id: '2', name: '陈二', phone: '12345678901', type: 2, avatarUrl: '/images/admin-icon.png'},
+      {_id: '3', name: '张三', phone: '12345678901', type: 2, avatarUrl: '/images/admin-icon.png'},
+    ],
+    roleMap: {0: '种植户', 1: '销售商', 2: '管理员'},
+    tabindex: 0,
+    searchValue: null,
+    // 点击按钮之后的相关弹窗按钮
+    isRemove: false,
+    isSetAdmin: false,
+    isSilent: false
+  },
+  onChangeTabs(event) {
+    const {index} = event.detail
+    const toShowData = index == 0 ? this.data.adminUsers : this.data.normalUsers
+    this.setData({
+      showData: toShowData,
+      tabindex: index
+    })
+  },
+  onSearch(event) {
+    const value = this.data.searchValue
+    if (value == null || value == '') {
+      this.setData({
+        showData: this.data.tabindex == 0 ? this.data.adminUsers : this.data.normalUsers
+      })
+      return
+    }
+    const curData = this.data.tabindex == 0 ? this.data.adminUsers : this.data.normalUsers
+    const toShowData = [];
+    for(var i = 0; i < curData.length; i ++) {
+      if(curData[i].name == value) {
+          toShowData.push(curData[i])
+      }
+    }
+    this.setData({
+      showData: toShowData,
+      searchValue: null
+    })
+  },
+  // 移除管理员
+  removeAdmin(event) {
+    const {removeAdminIndex} = event.target.dataset
+    const adminToRemove = this.data.adminUsers[removeAdminIndex]
+    console.log("remove " + adminToRemove._id + "-" + adminToRemove.name)
+    this.setData({
+      isRemove: true
+    })
+
+    Dialog.confirm({
+      selector: "#remove-admin",
+      title: '确认移除',
+      message: `是否移除管理员: ${adminToRemove.name}`,
+      width: '650rpx'
+    })
+      .then(() => {
+        // confirm
+        console.log('确认移除！')
+      })
+      .catch(() => {
+        // cancel
+        console.log('取消移除！')
+      })
+      .finally(() => {
+        this.setData({
+          isRemove: false
+        })
+      });
+  
+  },
+  // 新增管理员
+  setAdmin(event) {
+    const {setAdminIndex} = event.target.dataset
+    const adminToAdd = this.data.normalUsers[setAdminIndex]
+    this.setData({
+      isSetAdmin: true
+    })
+
+    Dialog.confirm({
+      selector: "#set-admin",
+      title: '确认添加',
+      message: `是否添加管理员: ${adminToAdd.name}？`,
+      width: '650rpx'
+    })
+      .then(() => {
+        // confirm
+        console.log('确认添加！')
+      })
+      .catch(() => {
+        // cancel
+        console.log('取消添加！')
+      })
+      .finally(() => {
+        this.setData({
+          isSetAdmin: false
+        })
+      });
+  },
+  // 禁言
+  setSilent(event) {
+    const {setSilentIndex} = event.target.dataset
+    const userToSilent = this.data.normalUsers[setSilentIndex]
+    this.setData({
+      isSilent: true
+    })
+
+    Dialog.confirm({
+      selector: "#set-silent",
+      title: '确认禁言',
+      message: `是否禁言用户: ${userToSilent.name}？`,
+      width: '650rpx'
+    })
+      .then(() => {
+        // confirm
+        console.log('确认禁言！')
+      })
+      .catch(() => {
+        // cancel
+        console.log('取消禁言！')
+      })
+      .finally(() => {
+        this.setData({
+          isSilent: false
+        })
+      });
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    this.setData({
+      showData: this.data.normalUsers
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
---
+++ miniprogram/pages/sellerOrder/sellerOrder.js
@@ -0,0 +1,162 @@
+// pages/farmerOrder/farmerOrder.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    farmerInfo: {},
+    sellerName: "xx公司",
+    wantedProducts: ['梨', '苹果'],
+    contactPerson: '陈先生',
+    phone: '191928347573',
+    places2sell: ['广州', '深圳'],
+    credits: 4.9,
+    unit: "箱",
+    viewAccount: 1090,
+    productDetails: [
+      {id: 1, name: 'a菇', amount: 10, expectedPrice: 32.7, description: '描述信息1'},
+      {id: 2, name: 'c菇', amount: 13, expectedPrice: 12, description: '描述信息2'},
+      {id: 3, name: 'c菇', amount: 13, expectedPrice: 12, description: '描述信息3'},
+    ],
+    showDialog: false,  // 展示下单弹窗
+    orderamount: null,  // 下单数量
+    targetOrder: null,  // 下单的产品
+    totalcost: null,
+    confirmOrder: false, // 是否已经确定下单
+  },
+  // 联系商家按钮
+  callPhone(event) {
+    wx.makePhoneCall({
+      phoneNumber: this.data.phone,
+    })
+  },
+
+  // 点击出售按钮
+  takeOrder(event) {
+    // 点击的订单条目index
+    const {orderindex} = event.target.dataset
+    const orderItem = this.data.productDetails[orderindex]
+    this.setData({
+      showDialog: true,
+      targetOrder: orderItem
+    })
+    // console.log(orderItem)
+  },
+
+  // 确定下单
+  confirmOrder(event) {
+    // 出售的数量
+    const amount = this.data.orderamount
+    if(amount === null) {
+      wx.showToast({
+        title: '请输入有效数字',
+        icon: "error"
+      })
+    } else if (amount > this.data.targetOrder.amount) {
+      wx.showToast({
+        title: '数量大于需求',
+        icon: "error"
+      })
+    } else {
+      // 总价格
+      const totalcost = this.data.orderamount * this.data.targetOrder.expectedPrice
+      this.setData({
+        confirmOrder: true,
+        totalcost: totalcost.toFixed(2)
+      })
+    }
+    this.setData({
+      showDialog: false
+    })
+  },
+
+  // 执行订单
+  executeOrder() {
+    this.setData({
+      confirmOrder: false
+    })
+
+    // TODO 修改订单信息
+    console.log("执行订单！")
+    this.setData({
+      orderamount: null
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    // 隐藏号码
+    const maskedPhone = this.data.phone.slice(0, 3) + '*'.repeat(5) + this.data.phone.slice(8);
+    // 信誉评价的颜色
+    const goodColor = 'rgb(213, 255, 203)'
+    const badColor = "rgb(248, 220, 143)"
+    var crediscolor = ""
+    if(this.data.credits >= 3.0) {
+      crediscolor = goodColor
+    } else {
+      crediscolor = badColor
+    }
+    
+    this.setData({
+      wantedProductStr: this.data.wantedProducts.join('，'),
+      places2sellStr: this.data.places2sell.join('，'),
+      maskedPhone: maskedPhone,
+      crediscolor: crediscolor
+    })
+  },
+  // TODO 浏览量+1
+  addViewAmount() {
+    console.log("page is view")
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+    this.addViewAmount()
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
